38ff54626b7926dbe7bd00c9bcfa1115e2f2d040
Dacian Spinu <dacian@Dacians-Mac-mini.local>
2016-06-01 17:48:15 +0300

    Using props to modify the view and also moved all the rendering from the main.js file to App.js for now.

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
-    return <div>Hia there</div>
+    let txt = this.props.txt;
+    let cat = this.props.cat;
+    return (
+      <div>
+        <h1>{txt}</h1>
+        <b>{cat}</b>
+      </div>
+    );
   }
 }
 
-export default App
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'This is the default prop value',
+  cat: 6
+}
+
+
+ReactDOM.render(
+  <App cat={62} txt="This is some text value"/>,
+  document.getElementById('app')
+)
