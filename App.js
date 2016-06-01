31feae0e23980e3c6fe1de177bca9562602e618a
Dacian Spinu <dacian@Dacians-Mac-mini.local>
2016-06-01 18:34:38 +0300

    Revert to main.js rendering and experimenting with owner-ownee relations in reactjs, plus added a style.css to test smth on safari

--- App.js
+++ App.js
@@ -3,32 +3,37 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      txt: ""
+    }
+    this.update = this.update.bind(this)
   }
-
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
   render() {
     let txt = this.props.txt;
     let cat = this.props.cat;
     return (
-      <div>
-        <h1>{txt}</h1>
-        <b>{cat}</b>
+      <div class="class">
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update}></input>
+      <h1>{props.txt}</h1>
+    </div>
+  )
 }
 
-App.defaultProps = {
-  txt: 'This is the default prop value',
-  cat: 6
-}
 
-
-ReactDOM.render(
-  <App cat={62} txt="This is some text value"/>,
-  document.getElementById('app')
-)
+export default App
